136436f692b8e70ccef3d93f9515b122a3f2fc66
rykumar13 <rykumar13@gmail.com>
2021-04-10 02:35:34 +0100

    hooking up api

--- src/data/data.js
+++ src/data/data.js
@@ -1,12 +1,22 @@
-const baseURL = "http://127.0.0.1:5000/";
+const fetch = require("node-fetch");
+let base64 = require("base-64");
+
+const baseURL = "http://35.176.138.183/";
+let username = "api_username";
+let password = "api_password";
+let headers = new fetch.Headers();
+
+//headers.append('Content-Type', 'text/json');
+headers.append(
+  "Authorization",
+  "Basic " + base64.encode(username + ":" + password)
+);
 
 export const getData = async () => {
   let data = await fetch(`${baseURL}`, {
     method: "GET",
-    mode: "no-cors",
-    headers: {
-      Accept: "application/json",
-    },
+    mode: 'no-cors',
+    headers: headers,
   });
   data = await data.json();
   return JSON.parse(data);

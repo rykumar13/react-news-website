db2ba8c4feae24b24784d38023622d576909cb69
rykumar13 <rykumar13@gmail.com>
2021-04-16 01:23:42 +0100

    security

--- src/data/data.js
+++ src/data/data.js
@@ -1,12 +1,12 @@
+import { getUsername, getPassword } from "../data/creds"
+
 const fetch = require("node-fetch");
 let base64 = require("base-64");
 
 const baseURL = "https://dcoiul8283.execute-api.eu-west-2.amazonaws.com/dev";
-let username = "news_app_user";
-let password = "news_app_password";
 let headers = new fetch.Headers();
 
-headers.append("Authorization","Basic " + base64.encode(username + ":" + password));
+headers.append("Authorization","Basic " + base64.encode(getUsername() + ":" + getPassword()));
 
 export const getData = async () => {
   let data = await fetch(`${baseURL}`, {

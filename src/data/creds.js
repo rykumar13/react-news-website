7112bab953d01ae7f348d27c3abdb684f3f43d61
rykumar13 <rykumar13@gmail.com>
2021-04-17 22:49:59 +0100

    cors issue

---
+++ src/data/creds.js
@@ -0,0 +1,10 @@
+export const USERNAME = "news_app_user";
+export const PASSWORD = "news_app_password";
+
+export const getUsername = () => {
+  return USERNAME;
+};
+
+export const getPassword = () => {
+  return PASSWORD;
+};
--- src/data/data.js
+++ src/data/data.js
@@ -1,19 +1,21 @@
-import { getUsername, getPassword } from "../data/creds"
-
+import { getUsername, getPassword } from "../data/creds";
 const fetch = require("node-fetch");
 let base64 = require("base-64");
 
 const baseURL = "https://dcoiul8283.execute-api.eu-west-2.amazonaws.com/dev";
 let headers = new fetch.Headers();
 
-headers.append("Authorization","Basic " + base64.encode(getUsername() + ":" + getPassword()));
+headers.append(
+  "Authorization",
+  "Basic " + base64.encode(getUsername() + ":" + getPassword())
+);
 
 export const getData = async () => {
   let data = await fetch(`${baseURL}`, {
     method: "GET",
-    mode: 'no-cors',
     headers: headers,
+    mode: "no-cors",
   });
   data = await data.json();
-  return data;
+  return JSON.parse(data);
 };
